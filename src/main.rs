c0e08fd1639d86fef14e96a29a449e2b8d67619c
MaxVanDijck <vandijcksm@gmail.com>
2024-02-25 15:01:14 +1300

    getting output in reverse order

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use winit::{
     event_loop::{EventLoop, ControlFlow},
     window::WindowBuilder,
@@ -5,51 +6,54 @@ use winit::{
 
 };
 
-use std::os::fd::{AsRawFd, RawFd};
+use rustix_openpty::openpty;
+use rustix::fd::AsRawFd;
+use std::os::unix::io::FromRawFd;
+use std::fs::File;
+use std::io::Write;
+use std::io::{BufRead, BufReader};
+use rustix_openpty::rustix::termios;
+use rustix_openpty::rustix::termios::{InputModes, OptionalActions};
+
 use std::process::Command;
-use nix::unistd::ForkResult;
-use nix::pty::forkpty;
-use nix::unistd::read;
-
-fn read_from_fd(fd: RawFd) -> Option<Vec<u8>> {
-    // https://linux.die.net/man/7/pipe
-    let mut read_buffer = [0; 65536];
-    let read_result = read(fd, &mut read_buffer);
-    match read_result {
-        Ok(bytes_read) => Some(read_buffer[..bytes_read].to_vec()),
-        Err(_e) => None,
-    }
-}
-
-unsafe fn spawn_pty_with_shell(default_shell: String) -> RawFd {
-    match forkpty(None, None) {
-        Ok(fork_pty_res) => {
-            let stdout_fd = fork_pty_res.master.as_raw_fd(); // primary
-            if let ForkResult::Child = fork_pty_res.fork_result {
-                // I'm the secondary part of the pty
-                Command::new(&default_shell)
-                    .spawn()
-                    .expect("failed to spawn");
-                std::thread::sleep(std::time::Duration::from_millis(2000));
-                std::process::exit(0);
-            }
-            stdout_fd
-        }
-        Err(e) => {
-            panic!("failed to fork {:?}", e);
-        }
-    }
-}
 
 fn main() {
     let default_shell = std::env::var("SHELL")
             .expect("could not find default shell from $SHELL");
     println!("{default_shell}");
+    let pty = openpty(None, None).expect("Creating pty failed");
+    let master_fd = pty.controller.as_raw_fd();
+    let slave_fd =  pty.user.as_raw_fd();
 
-    let stdout_fd = unsafe {spawn_pty_with_shell(default_shell)};
-    let mut read_buffer = vec![1];
+    if let Ok(mut termios) = termios::tcgetattr(&pty.controller) {
+        // Set character encoding to UTF-8.
+        termios.input_modes.set(InputModes::IUTF8, true);
+        let _ = termios::tcsetattr(&pty.controller, OptionalActions::Now, &termios);
+    }
+
+    let cmd = Command::new(default_shell).spawn().expect("failed to spawn");
+    let cmd = Command::new("ls").spawn().expect("failed to spawn");
+    let cmd = Command::new("rg").spawn().expect("failed to spawn");
 
 
+
+
+    let mut master_file = unsafe { File::from_raw_fd(slave_fd) };
+    master_file.flush().expect("Failed to flush the command to pty");
+
+    let master_file = unsafe { File::from_raw_fd(master_fd) };
+    let reader = BufReader::new(master_file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("Read line: {}", line),
+            Err(e) => println!("Error reading line: {}", e),
+        }
+    }
+
+
+
+
+    // WINDOW STUFF
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().with_title("Terminal").build(&event_loop).unwrap();
     event_loop.set_control_flow(ControlFlow::Poll);
@@ -68,18 +72,6 @@ fn main() {
                 ..
             } => {
                 println!("{event:?}");
-                match read_from_fd(stdout_fd) {
-                    Some(mut read_bytes) => {
-                    read_buffer.append(&mut read_bytes);
-                    println!("sum");
-                }
-                    None => {
-                    let mut copy_buffer = vec![0; read_buffer.capacity()];
-                    copy_buffer.copy_from_slice(&read_buffer);
-                    println!("{:?}", String::from_utf8(copy_buffer).unwrap());
-                    println!("non");
-                }
-                }
             },
             _ => ()
         }
